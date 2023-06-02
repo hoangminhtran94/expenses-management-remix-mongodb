8fd6cc79617ab1590b8610999d855c0a5dcd1a67
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-02 02:35:44 -0400

    update for netlify deployment

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
+  future: {
+    unstable_tailwind: true,
+    v2_errorBoundary: true,
+  },
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
 };
